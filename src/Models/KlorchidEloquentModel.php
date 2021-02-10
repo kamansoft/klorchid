a98f5b53d07685023b5c940732c9caa78c38603b
lemys lopez <lemyskaman@gmail.com>
2021-02-10 09:05:11 -0400

    klorchidtest edit working again, fixed klorchideloquent model

--- src/Models/KlorchidEloquentModel.php
+++ src/Models/KlorchidEloquentModel.php
@@ -5,6 +5,8 @@ namespace Kamansoft\Klorchid\Models;
 use Illuminate\Database\Eloquent\Model;
 use Kamansoft\Klorchid\Models\Contracts\KlorchidEloquentModelInterface;
 use Kamansoft\Klorchid\Models\Contracts\KlorchidModelInterface;
+use  Kamansoft\Klorchid\Models\Traits\KlorchidModelsTrait;
+use  Kamansoft\Klorchid\Models\Traits\KlorchidStatusTrait;
 
 
 class KlorchidEloquentModel extends Model implements KlorchidModelInterface, KlorchidEloquentModelInterface
