747d76fc20371a83bb4c2280535259f85db0e675
lemys lopez <lemyskaman@gmail.com>
2022-02-07 09:43:38 -0400

    added the use of helper method to retrive the param name that holds the model within the request

--- src/Http/Request/EntityDependantFormRequest.php
+++ src/Http/Request/EntityDependantFormRequest.php
@@ -28,7 +28,7 @@ abstract class EntityDependantFormRequest extends \Illuminate\Foundation\Http\Fo
         //TODO: this must be improoved
         //if there is a TypeError there must be a wrong binding needed for a request to work
         //return $this->route($this->entityRouteParamName());
-        $param_key='model';
+        $param_key=$this->entityRouteParamName();
         if (array_key_exists($param_key, $this->route()->parameters())) {
             return $this->route()->parameters()[$param_key];
             //return $this->loadModelRelations($this->route()->parameters()[$param_key]);
